0aa5c1c849b5077f81a01056ca71006d4af79b37
Jack Donaghy <187638989+JackWilliamDonaghy-Dev@users.noreply.github.com>
2026-02-03 16:44:39 +0000

    Material Design setup
    Person Class started

--- Version01/App.xaml.cs
+++ Version01/App.xaml.cs
@@ -13,5 +13,6 @@ namespace Version01
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
---
+++ Version01/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Xml.Linq;
+
+namespace Version01
+{
+    internal class Person
+    {
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public DateTime LastContacted { get; set; }
+        public int Frequency { get; set; }
+        
+
+        private int lastID = 1;
+
+        public Person(string name, int frequency, DateTime lastContacted)
+        {
+            Name = name;
+            Frequency = frequency;
+            LastContacted = lastContacted;
+
+            PersonID = lastID;
+            lastID++;
+        }
+
+        public Person(string name, int frequency)
+        {
+            Name = name;
+            Frequency = frequency;
+
+            LastContacted = DateTime.Now;
+            PersonID = lastID;
+            lastID++;
+        }
+
+
+        public Person() { }
+
+
+
+    }
+}
