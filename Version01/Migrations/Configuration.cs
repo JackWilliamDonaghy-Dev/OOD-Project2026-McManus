0a397efbd5a086690183900976759c498a727e34
Jack Donaghy <187638989+JackWilliamDonaghy-Dev@users.noreply.github.com>
2026-03-11 14:51:34 +0000

    Database Changes
    trying to get Context correct for migrations

---
+++ Version01/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Version01.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Version01.PersonData>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Version01.PersonData";
+        }
+
+        protected override void Seed(Version01.PersonData context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
